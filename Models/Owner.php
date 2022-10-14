841d33a1a8d1dd9a5ed8c4988d5133f1087635f2
matimaster1 <matitorres.mt@gmail.com>
2022-10-14 17:52:12 -0300

    Owner1

--- Models/Owner.php
+++ Models/Owner.php
@@ -1,6 +1,20 @@
 <?php
 
-    
+namespace Models;
+
+class Owner{
+
+    private $ownerId;
+    private $firstName;
+    private $lastName;
+    private $dni;
+    private $email;
+    private $password;
+    private $phoneNumber;
+    private $
+
+}
+
 
 
 ?>
