d837439e953c149b6ece59c76090c57e6b38a0bb
ManuGavioli <juangavioli@gmail.com>
2022-10-14 16:38:00 -0300

    Framework inicial

--- Config/Config.php
+++ Config/Config.php
@@ -2,10 +2,11 @@
 
 define("ROOT", dirname(__DIR__) . "/");
 //Path to your project's root folder
-define("FRONT_ROOT", "/Framework/");
+define("FRONT_ROOT", "/TpFinal_PetHero/");
 define("VIEWS_PATH", "Views/");
-define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
-define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
+define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "Styles/css/");
+define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "Styles/js/");
+define("IMG_PATH", FRONT_ROOT.VIEWS_PATH . "Styles/img/");
 ?>
 
 
