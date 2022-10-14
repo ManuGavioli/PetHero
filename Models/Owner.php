25f021b00ae854d1dff988944d3150fd331bd78b
matimaster1 <matitorres.mt@gmail.com>
2022-10-14 17:57:01 -0300

    Owner2

--- Models/Owner.php
+++ Models/Owner.php
@@ -11,8 +11,102 @@ class Owner{
     private $email;
     private $password;
     private $phoneNumber;
-    private $
+    private $pets;
 
+ 
+
+    public function getOwnerId()
+    {
+        return $this->ownerId;
+    }
+
+    public function setOwnerId($ownerId)
+    {
+        $this->ownerId = $ownerId;
+    }
+
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+  
+    public function getDni()
+    {
+        return $this->dni;
+    }
+
+
+    public function setDni($dni)
+    {
+        $this->dni = $dni;
+    }
+
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+
+    public function getPhoneNumber()
+    {
+        return $this->phoneNumber;
+    }
+
+
+    public function setPhoneNumber($phoneNumber)
+    {
+        $this->phoneNumber = $phoneNumber;
+
+    }
+
+    public function getPets()
+    {
+        return $this->pets;
+    }
+
+    public function setPets($pets)
+    {
+        $this->pets = $pets;
+
+    }
 }
 
 
